2ab67ff768f85250ea46a2c30fe8de33e371ccd3
Eric Nuanes <ecnuanes@outlook.com>
2018-04-11 22:18:22 -0700

    I updated the letter.js file to get the hangman game working.

--- letter.js
+++ letter.js
@@ -1,21 +1,22 @@
-var letter = function(ltr) {
-    // Property to store the actual letter
+var Letter = function(ltr) {
+    // property to store the actual letter
     this.letter = ltr;
-    // Property/boolean if the letter can be shown
+    // property/boolean if the letter can be shown
     this.appear = false;
-
+    
     this.letterRender = function() {
-        if(this.letter == ' ') { // Renders a blank as it is
-            // makes sure that when the function checks if a word is found doesn't read the blank as false
-            this.appear = true;
-            return ' '; 
-        } if(this.appear === false) { // if it doesn't appear it returns a '_'
-
-        } else { // Othewise it just appears as itself
-            return this.letter;
+        if(this.letter == ' ') { //renders a blank as it is
+          //makes sure that when the function checks if the word is found doesn't read the blank as false.
+          this.appear = true;
+          return '  ';
+        }if(this.appear === false) { //if it doesn't appear, it returns a ' _ '
+          return ' _ ';
+        } else { //otherwise it just appears as itself
+          return this.letter;
         }
+    
     };
 };
-
-// Export to use in word.js
+    
+// export to use in word.js
 module.exports = Letter;
